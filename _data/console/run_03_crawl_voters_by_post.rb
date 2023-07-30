9128f9370164e7829120c55a4733692eb11d429c
Quang <quang@local>
2023-07-30 15:15:22 +0700

    ok

--- _data/console/run_03_crawl_voters_by_post.rb
+++ _data/console/run_03_crawl_voters_by_post.rb
@@ -16,8 +16,8 @@
 
 # ssh root@159.89.192.33 'ls -1 run/tmp/voters-by-post/ | wc -l'
 
-# ssh root@159.89.192.33 "cd /root/run/ && mkdir done_05_b && find tmp/voters-by-post/ -name '*.json' -exec mv -t done_05_b/ {} + && zip -r done_05_b.zip done_05_b/"
-# scp root@159.89.192.33:/root/run/done_05_b.zip /Users/quang/Downloads/ok/voters-by-post/
+# ssh root@159.89.192.33 "cd /root/run/ && mkdir done_01_a && find tmp/voters-by-post/ -name '*.json' -exec mv -t done_01_a/ {} + && zip -r done_01_a.zip done_01_a/"
+# scp root@159.89.192.33:/root/run/done_01_a.zip /Users/quang/Downloads/ok/voters-by-post/
 
 
 commands = []
@@ -41,24 +41,24 @@ slipt_commands_to_files(commands, 15)
 def import_voters json_path="tmp/run/tmp/"
   Dir["#{json_path}/**/*.json".gsub("//","/")].shuffle.each do |fn|
     if File.exist?(fn) && !File.zero?(fn)
-      fn2 = fn.gsub(".json",".ongoing")
-      system "mv #{fn} #{fn2}"
-
-      data = JSON.parse(File.read(fn2))
+      data = parse_json(File.read(fn))
+      next if data.nil?
 
       if data["data"]
+        slug = fn.split("/").last.split(".json").first
         if post_data = helper_get_node_by_path(data, "data,post")
           post = helper_get_post_by_node_data(post_data)
+          post.fullscans_needed = nil if post.fullscans_needed == true
           post.save
           if post_data["contributors"].count > 0
-            system "rm #{fn2}"
+            system "rm #{fn}"
           end
+          Post.where("slug=? and id!=?", slug, post.id).delete_all
         else
-          slug = fn2.split("/").last.split(".ongoing").first
           if _post = Post.find_by(slug: slug)
             _post.is_trashed = true
             _post.save
-            system "rm #{fn2}"
+            system "rm #{fn}"
           end
         end
 
